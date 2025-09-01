40b600c800c33cb53b5b7b9c9bbe51999792f499
lemmiEphraim <lemmiabdissa@gmail.com>
2025-09-01 14:55:46 +0300

    Update index.tsx

--- prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
+++ prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
@@ -1,1 +1,9 @@
-
+// Sample of the modified component
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>**First App Created**</Text>
+      {/* ... rest of the component */}
+    </View>
+  );
+}
